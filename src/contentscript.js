b7716ff04365a7dcfcbfe87abb644d27e1b95025
Chi-En Wu <jason2506.mail@gmail.com>
2014-02-02 14:12:18 +0800

    Follow some coding style

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,6 +1,6 @@
 (function() {
 
-const videoTypes = {
+const VIDEO_TYPES = {
     'FLV': [
         { i: '5', n: '224p' },
         { i: '6', n: '270p' },
@@ -49,41 +49,50 @@ const videoTypes = {
     ]
 };
 
-const fmtStreamMapPattern = /"url_encoded_fmt_stream_map": "([^"]+)"/;
-const adaptiveFmtStreamMapPattern = /"adaptive_fmts": "([^"]+)"/;
-const fmtITagPattern = /itag=(\d+)/;
-const fmtUrlPattern = /url=([^&]+)/;
-const fmtSigPattern = /s=([^&]+)/;
+const FMT_STREAM_MAP_PTN = /"url_encoded_fmt_stream_map": "([^"]+)"/;
+const ADAPTIVE_FMT_STREAM_MAP_PTN = /"adaptive_fmts": "([^"]+)"/;
+const FMT_ITAG_PTN = /itag=(\d+)/;
+const FMT_URL_PTN = /url=([^&]+)/;
+const FMT_SIG_PTN = /s=([^&]+)/;
 
 function showDownloadLinks(title, fmtUrlList) {
-    var links = $('<ul>').attr('id', 'download-list');
-    for (var type in videoTypes) {
-        var videoList = videoTypes[type].filter(function (fmt) {
-            return fmt.i in fmtUrlList;
-        });
+    var links,
+        type,
+        videoList,
+        inUrlList,
+        item,
+        index,
+        fmt,
+        anchor,
+        button,
+        panel;
 
-        if (videoList.length > 0) {
-            item = $('<li>').attr({
-                'style': 'padding: 3px 10px'
-            });
+    inUrlList = function(fmt) {
+        return fmt.i in fmtUrlList;
+    };
 
-            item.append($('<span>').attr({
-                'style': 'display: inline-block; width: 120px'
-            }).append(type + ':'));
+    links = $('<ul>').attr('id', 'download-list');
+    for (type in VIDEO_TYPES) {
+        videoList = VIDEO_TYPES[type].filter(inUrlList);
+        if (!videoList.length) { continue; }
 
-            for (var index in videoList) {
-                var fmt = videoList[index];
-                item.append($('<a>').attr({
-                    'href': fmtUrlList[fmt.i] + '&title=' + title,
-                    'style': 'display: inline-block; width: 50px'
-                }).append(fmt.n));
-            }
+        item = $('<li>').attr({'style': 'padding: 3px 10px'});
+        item.append($('<span>').attr({
+            'style': 'display: inline-block; width: 120px'
+        }).append(type + ':'));
 
-            links = links.append(item);
+        for (index in videoList) {
+            fmt = videoList[index];
+            item.append($('<a>').attr({
+                'href': fmtUrlList[fmt.i] + '&title=' + title,
+                'style': 'display: inline-block; width: 50px'
+            }).append(fmt.n));
         }
+
+        links = links.append(item);
     }
 
-    var panel = $('<div>').attr({
+    panel = $('<div>').attr({
         'id': 'action-panel-download',
         'class': 'action-panel-content hid',
         'data-panel-loaded': 'true'
@@ -93,17 +102,18 @@ function showDownloadLinks(title, fmtUrlList) {
     }).append(links));
     $('#action-panel-details').after(panel);
 
-    var anchor = $('#watch7-secondary-actions').find('span').first();
-    var botton = anchor.clone();
-    botton.find('button')
+    anchor = $('#watch7-secondary-actions').find('span').first();
+    button = anchor.clone();
+    button.find('button')
         .attr('data-trigger-for', 'action-panel-download')
         .removeClass('yt-uix-button-toggled');
-    botton.find('span').text('下載');
-    anchor.after(botton);
+    button.find('span').text('下載');
+    anchor.after(button);
 }
 
 function extractTitle() {
-    return encodeURIComponent($('#eow-title').attr('title')).replace(/%20/g, '+');
+    var title = $('#eow-title').attr('title');
+    return encodeURIComponent(title).replace(/%20/g, '+');
 }
 
 function extractSig(s) {
@@ -111,25 +121,30 @@ function extractSig(s) {
 }
 
 function extractUrl(text) {
-    var urlMatch = fmtUrlPattern.exec(text);
-    return unescape(urlMatch[1]);
+    var urlMatch = FMT_URL_PTN.exec(text);
+    return decodeURIComponent(urlMatch[1]);
 }
 
 function extractUrlWithSig(text) {
-    var sigMatch = fmtSigPattern.exec(text);
-    sig = extractSig(sigMatch[1]);
+    var sigMatch = FMT_SIG_PTN.exec(text),
+        sig = extractSig(sigMatch[1]);
     return extractUrl(text) + '&signature=' + sig;
 }
 
 function createFmtUrlList(fmtStreamMap, urlExtractor, fmtUrlList) {
-    var fmtStreamList = fmtStreamMap
+    var fmtStreamList,
+        index,
+        text,
+        itagMatch;
+
+    fmtStreamList = fmtStreamMap
         .replace(/\\u0026/g, '&')
         .split(',');
 
     fmtUrlList = fmtUrlList || {};
-    for (var index in fmtStreamList) {
-        var text = fmtStreamList[index];
-        var itagMatch = fmtITagPattern.exec(text);
+    for (index in fmtStreamList) {
+        text = fmtStreamList[index];
+        itagMatch = FMT_ITAG_PTN.exec(text);
         fmtUrlList[itagMatch[1]] = urlExtractor(text);
     }
 
@@ -137,14 +152,20 @@ function createFmtUrlList(fmtStreamMap, urlExtractor, fmtUrlList) {
 }
 
 $(document).ready(function() {
-    var title = extractTitle();
-    var script = $('script:contains(\'"url_encoded_fmt_stream_map"\')')[0].text;
-    var fmtUrlList = {};
+    var title,
+        script,
+        fmtUrlList,
+        fmtStreamMap,
+        adaptiveFmtStreamMap;
 
-    var fmtStreamMap = fmtStreamMapPattern.exec(script)[1];
+    title = extractTitle();
+    script = $('script:contains(\'"url_encoded_fmt_stream_map"\')')[0].text;
+    fmtUrlList = {};
+
+    fmtStreamMap = FMT_STREAM_MAP_PTN.exec(script)[1];
     createFmtUrlList(fmtStreamMap, extractUrlWithSig, fmtUrlList);
 
-    var adaptiveFmtStreamMap = adaptiveFmtStreamMapPattern.exec(script)[1];
+    adaptiveFmtStreamMap = ADAPTIVE_FMT_STREAM_MAP_PTN.exec(script)[1];
     createFmtUrlList(adaptiveFmtStreamMap, extractUrlWithSig, fmtUrlList);
 
     showDownloadLinks(title, fmtUrlList);
